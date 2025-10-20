0785f1057b541fec0ed9d113db696fcd5dae88a6
Jazmin Santacruz <jazmin05santacruz@gmail.com>
2025-10-19 22:03:32 -0300

    Update settings.py

--- psis_api/settings.py
+++ psis_api/settings.py
@@ -187,7 +187,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     # agrega el front cuando lo publiques:
-    # "https://<tu-frontend>.onrender.com",
+    "https://psis-inscripciones-frontend.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
